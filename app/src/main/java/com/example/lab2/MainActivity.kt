c21a3da08be90b5741090e7aff44c3123bfc3dc9
Olga <Vasileva.o.p.2.0.0@gmail.com>
2024-10-10 12:28:18 +0700

    2 лабораторная

--- app/src/main/java/com/example/lab2/MainActivity.kt
+++ app/src/main/java/com/example/lab2/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.example.lab2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editText: EditText
+    private lateinit var button: Button
+    private lateinit var sumTextView: TextView
+    private lateinit var lastTermTextView: TextView
+    private lateinit var iterationsTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        editText = findViewById(R.id.editTextText2)
+        button = findViewById(R.id.button2)
+        sumTextView = findViewById(R.id.textView6)
+        lastTermTextView = findViewById(R.id.textView7)
+        iterationsTextView = findViewById(R.id.textView8)
+
+        button.setOnClickListener {
+            val n = editText.text.toString().toIntOrNull()
+
+
+        }
     }
 }
+
+
